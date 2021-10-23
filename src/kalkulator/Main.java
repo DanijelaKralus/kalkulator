ef579f88381497058791a152d15cb3ef51e7440c
Danijela Kralus <kralusdanijela@gmail.com>
2021-10-23 22:36:38 +0200

    Initial read line

--- src/kalkulator/Main.java
+++ src/kalkulator/Main.java
@@ -1,10 +1,16 @@
 package kalkulator;
 
+import com.sun.java_cup.internal.runtime.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		java.util.Scanner scr = new java.util.Scanner(System.in);
+		String komanda="";
+		do {	
+		komanda=scr.nextLine();
+		} while (!komanda.equals("End"));
 	}
 
 }
